502689cc41b7958c28fdaab8a7fca3913203a2ef
alitaso345 <alice.maru345@gmail.com>
2020-03-07 23:35:06 +0900

    Almost done

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alitaso345/unagi
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/alitaso345/azusaconverter v0.0.0-20200307135148-71bed6c3df94
+	github.com/gin-gonic/gin v1.5.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"github.com/alitaso345/azusaconverter"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
+
+type postRequest struct {
+	Content string `json:content`
+}
+
+// 佐々木梓の台詞には「[azusa:久美子さ、なんで南宇治高校に行かんかったん？]」のようなアノテーションがついている前提
+func convertAnnotation(text string) string {
+	converted := ""
+	for _, line := range strings.Split(text, "\n") {
+		r := regexp.MustCompile(`[azusa:*]`)
+		if r.MatchString(line) {
+			line = strings.Replace(line, "[azusa:", "", 1)
+			line = strings.Replace(line, "]", "", 1)
+			converted += azusaconverter.Convert(line)
+		} else {
+			converted += line
+		}
+		converted += "\n"
+	}
+	return converted
+}
 
 func main() {
 	r := gin.New()
@@ -16,8 +40,12 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 	r.POST("/convert", func(c *gin.Context) {
+		requestBody := postRequest{}
+		c.Bind(&requestBody)
+
+		result := convertAnnotation(requestBody.Content)
 		c.JSON(http.StatusOK, gin.H{
-			"message": "佐々木梓さんお誕生日おめでとうございます",
+			"message": result,
 		})
 	})
 
