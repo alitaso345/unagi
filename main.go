3911a6f8fde27cabec0c4b0786783c8c798a3e41
alitaso345 <alice.maru345@gmail.com>
2020-03-07 22:28:42 +0900

    Add js

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
 	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.LoadHTMLGlob("templates/*.tmpl.html")
+
 	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+	r.POST("/convert", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "佐々木梓さんお誕生日おめでとうございます",
 		})
 	})
 
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
 	r.Run(":" + port)
 }
